7466223d86af4c77581a36ecef7074b2f8b618e4
rookedsysc <rookedsysc36@gmail.com>
2024-06-22 08:34:46 +0900

    feat: Unknown Exception 처리

--- accounts/src/main/kotlin/org/rookedsysc/accounts/accounts/exception/GlobalExceptionHandler.kt
+++ accounts/src/main/kotlin/org/rookedsysc/accounts/accounts/exception/GlobalExceptionHandler.kt
@@ -11,6 +11,21 @@ import java.time.LocalDateTime
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
+    @ExceptionHandler(value = [Exception::class])
+    fun handleGlobalException(e: Exception, webRequest: WebRequest): ResponseEntity<ErrorResponseDto> {
+        val httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
+
+        val response = ErrorResponseDto(
+                apiPath = webRequest.getDescription(false)
+                        .toString(),
+                errorCode = httpStatus,
+                errorMsg = "Unknown error occurred. Please contact support",
+                errorTime = LocalDateTime.now()
+        )
+        return ResponseEntity(response, httpStatus)
+    }
+
+
     @ExceptionHandler(value = [CustomerAlreadyExistException::class])
     fun handleCustomerAlreadyExistException(e: CustomerAlreadyExistException, webRequest: WebRequest): ResponseEntity<ErrorResponseDto> {
         val httpStatus = HttpStatus.BAD_REQUEST
