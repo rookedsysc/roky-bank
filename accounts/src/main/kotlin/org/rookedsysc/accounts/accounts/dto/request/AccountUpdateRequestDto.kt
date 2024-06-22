f9df5b934ce74845855e4bf1d554d9d18ab56bef
rookedsysc <rookedsysc36@gmail.com>
2024-06-22 10:34:03 +0900

    refactor: 요구사항에 맞게 NullOrEmail, NullOrName validation 커스텀 개발 및 적용

--- accounts/src/main/kotlin/org/rookedsysc/accounts/accounts/dto/request/AccountUpdateRequestDto.kt
+++ accounts/src/main/kotlin/org/rookedsysc/accounts/accounts/dto/request/AccountUpdateRequestDto.kt
@@ -6,11 +6,9 @@ import jakarta.validation.constraints.Pattern
 data class AccountUpdateRequestDto(
         @field:NotEmpty(message = "AccountNumber can not be a null or empty")
         @field:Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
-        val accountNumber: Long?,
+        val accountNumber: Long,
 
-        @field:NotEmpty(message = "AccountType can not be a null or empty")
         val accountType: String?,
 
-        @NotEmpty(message = "BranchAddress can not be a null or empty")
         val branchAddress: String?,
 )
--- accounts/src/main/kotlin/org/rookedsysc/accounts/accounts/dto/request/CustomerUpdateRequestDto.kt
+++ accounts/src/main/kotlin/org/rookedsysc/accounts/accounts/dto/request/CustomerUpdateRequestDto.kt
@@ -1,21 +1,20 @@
 package org.rookedsysc.accounts.accounts.dto.request
 
-import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
+import org.rookedsysc.accounts.accounts.service.validation.NullOrEmailValid
+import org.rookedsysc.accounts.accounts.service.validation.NullOrNameValid
 
 
 data class CustomerUpdateRequestDto(
-        @field:NotBlank(message = "Name cannot be blank.")
-        @field:Size(min = 5, max = 30, message = "Name must be between 5 and 30 characters.")
+        @field:NullOrNameValid
         val name: String?,
-        
-        @field:NotBlank(message = "Email cannot be blank.")
-        @field:Email(message = "Email addr should be a valid value.")
+
+        @field:NullOrEmailValid
         val email: String?,
 
-        @Pattern(regexp = "(^$|[0-9]{10})", message = "Password must contain at least one digit, one lowercase, one uppercase, and 8 characters.")
+        @field:Pattern(regexp = "(^$|[0-9]{10})", message = "Password must contain at least one digit, one lowercase, one uppercase, and 8 characters.")
         @field:Size(min = 10, max = 10, message = "Mobile number must be 10 digits.")
-        val mobileNumber: String?,
+        val mobileNumber: String,
 )
