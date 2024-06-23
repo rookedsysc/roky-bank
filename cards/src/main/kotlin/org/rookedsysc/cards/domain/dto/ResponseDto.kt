fdbe75525fcaf2796d7bb515d63373f0e0a8c488
rookedsysc <rookedsysc36@gmail.com>
2024-06-23 23:00:58 +0900

    feat: Schema 추가

--- cards/src/main/kotlin/org/rookedsysc/cards/domain/dto/ResponseDto.kt
+++ cards/src/main/kotlin/org/rookedsysc/cards/domain/dto/ResponseDto.kt
@@ -1,6 +1,19 @@
 package org.rookedsysc.cards.domain.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(
+        name = "Response",
+        description = "Schema to hold successful response information"
+)
 data class ResponseDto(
+        @Schema(
+                description = "Status code in the response"
+        )
         val status: String,
+
+        @Schema(
+                description = "Status message in the response"
+        )
         val statusMsg: String
 )
